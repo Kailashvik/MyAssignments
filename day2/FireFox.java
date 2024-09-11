0131da15e91219ef3242ee0945e092c79218c86b
Kailashvik <kailashvicky80@gmail.com>
2024-09-11 09:49:09 +0530

    Week1.Day2
    Data types

---
+++ day2/FireFox.java
@@ -0,0 +1,21 @@
+package week1.day2;
+
+public class FireFox {
+
+	public static void main(String[] args) {
+		
+		//Datatypes
+		
+		String browserName = "Google";
+		float browserVersion =91.f;
+		boolean isVisible = false;
+		int releaseYear = 2008;
+		char browserLogo = 'C';
+		
+		System.out.println(" The browser name is " + browserName + " and it is released in " + releaseYear +
+				" ,the current version is " + browserVersion + " . logo - " + browserLogo 
+				+ " is it beta? " + isVisible );
+
+	}
+
+}
